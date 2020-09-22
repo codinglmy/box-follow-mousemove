eb861a109d37af7ff6b3d140e002a56956ae840b
codingLmy <67003602@qq.com>
2020-09-22 13:52:08 +0800

    实现拖动效果，即鼠标按下时div跟随鼠标移动，鼠标松开后停止移动

---
+++ main.js
@@ -0,0 +1,32 @@
+//简化querySelector
+function $(selector){
+    return document.querySelector(selector)
+}
+
+const box = $('#box')
+const body = $('body')
+
+box.style.left = localStorage.getItem('posX') - 50 + "px"
+box.style.top = localStorage.getItem('posY') - 50 + "px" 
+
+let isDown = false
+
+box.addEventListener('mousedown',()=>{
+    isDown = true
+})
+box.addEventListener('mouseup',()=>{
+    isDown = false
+})
+
+box.addEventListener('mousemove',(e)=>{
+    if(isDown){
+        let x = e.pageX
+        let y = e.pageY
+        console.log(x + "," +y)
+        box.style.left = x - 50 + "px"
+        box.style.top = y - 50 + "px" 
+        localStorage.setItem('posX',x)
+        localStorage.setItem('posY',y)
+    }
+})
+
